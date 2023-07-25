58e537273461873b522ff3ddce1999161f5035e9
Manpreet Kaur <parminder@gamechangesns.com>
2023-07-25 22:11:45 +0530

    search box

--- src/Home.js
+++ src/Home.js
@@ -39,8 +39,21 @@ function Home(){
   return (
 
     <div className="Home">
-      <h1>WEATHER</h1>
-      <div className="glassBox">
+      <div className="navbar-nav">
+        <div className="navbar-brand">WEATHER</div>
+        <div className="navbar-center"> 
+            <input type="text" 
+            placeholder='search city'
+            className="searchBox"
+
+              />
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007
+             1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+        </div>
+      </div>
+      {/* <div className="glassBox">
 
       <input
         type="text"
@@ -50,7 +63,7 @@ function Home(){
        <br/>
         <button onClick={processInput}>Output</button>
         <br/>
-        <p>{outputValue}</p>
+        <p>{outputValue}</p> */}
             
     
 
@@ -58,7 +71,7 @@ function Home(){
 
 
         </div>
-          </div>
+        //   </div>
 
         )
 }
